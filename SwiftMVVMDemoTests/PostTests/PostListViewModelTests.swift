4fdafde9bf6c97066f2f699b66f413f1d8c2e27d
Rushi Sangani <rushisangani@gmail.com>
2024-01-10 20:55:50 +0100

    updated tests for PostListViewModel

--- SwiftMVVMDemoTests/PostTests/PostListViewModelTests.swift
+++ SwiftMVVMDemoTests/PostTests/PostListViewModelTests.swift
@@ -6,17 +6,21 @@
 //
 
 import XCTest
+import Combine
 @testable import SwiftMVVMDemo
 
 final class PostListViewModelTests: XCTestCase {
     var postListViewModel: PostListViewModel?
+    private var cancellables: Set<AnyCancellable>!
     
     override func setUpWithError() throws {
         postListViewModel = PostListViewModel(postService: MockPostService())
+        cancellables = []
     }
 
     override func tearDownWithError() throws {
         postListViewModel = nil
+        cancellables = nil
     }
 
     func testPostViewModelReturnsPosts() async throws {
@@ -33,6 +37,24 @@ final class PostListViewModelTests: XCTestCase {
         let last = try XCTUnwrap(posts.last)
         XCTAssertEqual(last.title, "at nam consequatur ea labore ea harum")
         XCTAssertEqual(last.body, "cupiditate quo est a modi nesciunt soluta\nipsa voluptas error itaque dicta in\nautem qui minus magnam et distinctio eum\naccusamus ratione error aut")
+    }
+    
+    func testPostsProperty() async throws {
+        let expectation = XCTestExpectation(description: "@Published posts count")
+        
+        postListViewModel!
+            .$posts
+            .dropFirst()
+            .sink(receiveValue: { posts in
+                // verify count and fullfil the expectation
+                XCTAssertEqual(posts.count, 100)
+                expectation.fulfill()
+            })
+            .store(in: &cancellables)
+        
+        try await postListViewModel!.loadPosts()
+        
+        wait(for: [expectation], timeout: 3)
     }
     
     func testPostViewModelFailedGettingPosts() {
